e32f6d6abbe03ccf3b29846c6142e371865efce3
Jamkris <dltmdgus1412@gmail.com>
2023-08-15 01:43:09 +0900

    ADD :: 스토리 버튼 움직임 완성

--- src/docs/Components/Story/Story.style.jsx
+++ src/docs/Components/Story/Story.style.jsx
@@ -7,41 +7,42 @@ export const StoryContainer = s.div`
     user-select:none;
 
     font-family: 'Raleway', sans-serif;
+    
     margin-left: 20vw;
     width: 1105px;
     text-align: center;
     background-color: red;
-    overflow-x: hidden;
-`
+
+    display: flex;
+
+`;
 
 export const Items = s.div`
+    overflow-x: scroll;
+    scroll-behavior: smooth;
+    &::-webkit-scrollbar {
+    display: none;
+    }
     display: flex;
-`
+`;
 
 export const Item = s.div`
     margin-right: 30px;
     width: 95px
-`
+`;
 
 export const icon = s.img`
     width: 66px;
     border-radius: 100%;
     border: 0.5px solid lightgrey;
-`
+`;
 
 export const ItemText = s.div`
     
+`;
+
+export const LeftButton = s.button`
 `
 
-export const LButton = s.button`
-    position: absolute;
-`
-
-export const RButton = s.button`
-    position: absolute;
-    right: 1000px;
-
-    &active {
-        transform: translate(300px);
-    }
+export const RightButton = s.button`
 `
